210048ee6e3c6592dc4ee08e3b64b28b88e873f8
Johannes Schweig <johannes.schweig@maibornwolff.de>
2024-02-07 17:22:53 +0100

    deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# abort on errors
-set -e
-
-cd dist && cp index.html 404.html && cd .. 
-
-git subtree push --prefix dist origin gh-pages
--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  moduleNameMapper: {
-    '\\.(pdf|css)': '<rootDir>/empty.js'
-  }
-}
