a4c3bf536211f7a0ab0dbba9a29e52fb62269831
Johannes Schweig <johannes.schweig@maibornwolff.de>
2024-02-07 16:38:37 +0100

    changed gitignore and deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,36 +3,6 @@
 # abort on errors
 set -e
 
-# build
-npm run build
+cd dist && cp index.html 404.html && cd .. 
 
-# navigate into the build output directory
-cd dist
-# copy index.html for routes
-# rm -r phoenix greenbox querybuilder codesnippets knimehub legal
-mkdir phoenix greenbox querybuilder codesnippets knimehub mechanics thesis legal about scenariomanager esgehtlos trafficticket
-cp index.html phoenix
-cp index.html greenbox
-cp index.html querybuilder
-cp index.html codesnippets
-cp index.html knimehub
-cp index.html mechanics
-cp index.html scenariomanager
-cp index.html esgehtlos
-cp index.html thesis
-cp index.html trafficticket
-cp index.html about
-cp index.html legal
-# copy README.md over
-cp ../README.md ./
-# create CNAME file to prevent overwritten custom domain on deploy
-# see https://github.com/tschaub/gh-pages/issues/213
-echo "johannesschweig.de" >> CNAME
-
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:johannesschweig/johannesschweig.github.io.git main
-
-cd -
+git subtree push --prefix dist origin gh-pages
